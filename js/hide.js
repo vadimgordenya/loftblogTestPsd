5df6614502ed5a3b7806d9f53ceb7b7b01ded38b
Vadim <vadimgordenya@gmail.com>
2016-12-14 00:59:43 +0300

    Update hide.js
    update

--- js/hide.js
+++ js/hide.js
@@ -5,80 +5,80 @@ $(document).ready(function() {
 $('#js__cheked').change(function () {
 	$('.cheked__text1').css('color','green').css('font-weight', 'bold');
 	$('.text__about-js').css('background-color', '#ecf4e2');
+	count--;
 	setTimeout(function (){
 		$('.list__js').hide();
-	}, 1000);
-	count--;
-	if(count == 0) {
+		if(count === 0) {
 		$('.complite').css('display','block');
 		$('.courses__title').css('display','none');
 		$('.courses__about').css('display','none');
 	}
+	}, 1000);
 });
 $('#jq__cheked').change(function () {
 	$('.cheked__text2').css('color','green').css('font-weight', 'bold');
 	$('.text__about-jq').css('background-color', '#ecf4e2');
+	count--;
 	setTimeout(function (){
 		$('.list__jq').hide();
-	}, 1000);
-	count--;
-	if(count == 0) {
+		if(count === 0) {
 		$('.complite').css('display','block');
 		$('.courses__title').css('display','none');
 		$('.courses__base').css('display','none');
 	}
+	}, 1000);
 });
 $('#base__cheked').change(function () {
 	$('.cheked__text3').css('color','green').css('font-weight', 'bold');
 	$('.text__about-base').css('background-color', '#ecf4e2');
+	count--;
 	setTimeout(function (){
 		$('.list__base').hide();
-	}, 1000);
-	count--;
-	if(count == 0) {
+		if(count === 0) {
 		$('.complite').css('display','block');
 		$('.courses__title').css('display','none');
 		$('.courses__about').css('display','none');
 	}
+	}, 1000);
 });
 $('#respons1__cheked').change(function () {
 	$('.cheked__text4').css('color','green').css('font-weight', 'bold');
 	$('.text__about-respons').css('background-color', '#ecf4e2');
+	count--;
 	setTimeout(function (){
 		$('.list__respons').hide();
-	}, 1000);
-	count--;
-	if(count == 0) {
+		if(count === 0) {
 		$('.complite').css('display','block');
 		$('.courses__title').css('display','none');
 		$('.courses__about').css('display','none');
 	}
+	}, 1000);
 });
 $('#respons2__cheked').change(function () {
 	$('.cheked__text5').css('color','green').css('font-weight', 'bold');
 	$('.text__about-btn').css('background-color', '#ecf4e2');
+	count--;
 	setTimeout(function (){
 		$('.list__btn').hide();
-	}, 1000);
-	count--;
-	if(count == 0) {
+		if(count === 0) {
 		$('.complite').css('display','block');
 		$('.courses__title').css('display','none');
 		$('.courses__about').css('display','none');
 	}
+	}, 1000);
 });
 $('#respons3__cheked').change(function () {
 	$('.cheked__text6').css('color','green').css('font-weight', 'bold');
 	$('.text__about-gallery').css('background-color', '#ecf4e2');
+	count--;
 	setTimeout(function (){
 		$('.list__gallery').hide();
-	}, 1000);
-	count--;
-	if(count == 0) {
+		if(count === 0) {
 		$('.complite').css('display','block');
 		$('.courses__title').css('display','none');
 		$('.courses__about').css('display','none');
 	}
+	}, 1000);
 });
 
 
